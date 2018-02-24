2bbaafabdba8c09bd7a85baa9397b28c8626ac97
Pinae <jonny@pinae.net>
2018-02-24 18:38:00 +0100

    Show current time in time selector

--- time_selector_widget.py
+++ time_selector_widget.py
@@ -57,6 +57,15 @@ class TimeSelector(QWidget):
             label_height = font_metrics.height()
             r = min(w, h) / 2 - 0.6 * label_height
             alpha = i * pi / 6
+            if self.time and self.time.hour == i:
+                qp.setPen(QColor(0, 0, 0))
+                qp.setBrush(QColor(0, 0, 0))
+                qp.drawEllipse(int(round(w / 2 + r * sin(alpha) - 0.8 * label_height + 0.4)),
+                               int(round(h / 2 - r * cos(alpha) - 0.8 * label_height + 0.8)),
+                               int(round(1.6*label_height)), int(round(1.6*label_height)))
+                qp.setPen(QColor(255, 255, 255))
+            else:
+                qp.setPen(QColor(0, 0, 0))
             qp.drawText(int(round(w / 2 + r * sin(alpha) - label_width / 2)),
                         int(round(h / 2 - r * cos(alpha) + label_height / 2 - 3)),
                         str(i))
@@ -72,19 +81,37 @@ class TimeSelector(QWidget):
             label_height = font_metrics.height()
             r = min(w, h) / 2 - 1.8 * label_height
             alpha = i * pi / 6
+            if self.time and self.time.hour == i:
+                qp.setPen(QColor(0, 0, 0))
+                qp.setBrush(QColor(0, 0, 0))
+                qp.drawEllipse(int(round(w / 2 + r * sin(alpha) - 0.8 * label_height + 0.4)),
+                               int(round(h / 2 - r * cos(alpha) - 0.8 * label_height + 0.8)),
+                               int(round(1.6*label_height)), int(round(1.6*label_height)))
+                qp.setPen(QColor(255, 255, 255))
+            else:
+                qp.setPen(QColor(0, 0, 0))
             qp.drawText(int(round(w / 2 + r * sin(alpha) - label_width / 2)),
                         int(round(h / 2 - r * cos(alpha) + label_height / 2 - 3)),
                         str(i))
+        qp.setPen(QColor(0, 0, 0))
         qp.setBrush(QColor(0, 0, 0))
         qp.drawEllipse(int(round(w / 2 - (min(w, h) - 4.9 * font_metrics.height()) / 2)),
                        int(round(h / 2 - (min(w, h) - 4.9 * font_metrics.height()) / 2)),
                        min(w, h) - 4.9 * font_metrics.height(),
                        min(w, h) - 4.9 * font_metrics.height())
+        r = min(w, h) / 2 - 3.3 * font_metrics.height()
+        if self.time:
+            qp.setPen(QColor(128, 128, 128))
+            qp.setBrush(QColor(128, 128, 128))
+            alpha = self.time.minute * pi / 30
+            qp.drawEllipse(int(round(w / 2 + r * sin(alpha) - 0.8 * font_metrics.height() + 0.4)),
+                           int(round(h / 2 - r * cos(alpha) - 0.8 * font_metrics.height() + 0.8)),
+                           int(round(1.6 * font_metrics.height())),
+                           int(round(1.6 * font_metrics.height())))
         qp.setPen(QColor(255, 255, 255))
         for i in range(0, 60, 5):
             label_width = font_metrics.width(str(i))
             label_height = font_metrics.height()
-            r = min(w, h) / 2 - 3.3 * label_height
             alpha = i * pi / 30
             qp.drawText(int(round(w / 2 + r * sin(alpha) - label_width / 2)),
                         int(round(h / 2 - r * cos(alpha) + label_height / 2 - 3)),
@@ -94,8 +121,10 @@ class TimeSelector(QWidget):
 
     def hour_changed(self, new_value):
         self.time = time(hour=new_value, minute=self.time.minute if type(self.time) == time else 0)
+        self.update()
         self.time_changed.emit(self.time)
 
     def minute_changed(self, new_value):
         self.time = time(hour=self.time.hour if type(self.time) == time else 0, minute=new_value)
+        self.update()
         self.time_changed.emit(self.time)
